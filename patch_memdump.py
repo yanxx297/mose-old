2edc850449b64ad04a29c935ecbbd89e2d32dd52
yanxx297 <yanxx297@umn.edu>
2019-11-11 23:52:35 -0600

    Improve the efficiency of patch_memdump.py using hash table Instead of searching 1G data for each tainted page, store all the tainted pages in a hash map, and only go through the 1G data for one pass. Part of the previous implementation is no longer used and removed.

--- patch_memdump.py
+++ patch_memdump.py
@@ -11,21 +11,19 @@
 
 import sys
 
-
-def search_memdump(dumpfile, taint_page, offset = 0, page_size = 4096):
-    with open(dumpfile, "rb+") as dump:
-        res = []
-        dump.seek(offset);
+def load_file_to_hashmap(filename, page_size = 4096):
+    res = {}
+    with open(filename, "rb+") as fd:
         while True:
-            memdump_page = dump.read(page_size)
-            if memdump_page == "":
+            data = fd.read(page_size)
+            if data == "":
                 break
-            if memdump_page == taint_page:
-                res.append(dump.tell() - page_size)
-        return res
+            else:
+                res[data] = fd.tell() - page_size
+    return res
 
 
-def patch_memdump(dumpfile, exploitfile, taint_map, offset = 0, page_size = 4096):
+def patch_memdump(dumpfile, exploitfile, taint, offset = 0, page_size = 4096):
     with open(dumpfile, "rb+") as dump, open(dumpfile + "_patched", "wb") as out:
         count = 0
         out.write(dump.read(offset))
@@ -35,10 +33,10 @@ def patch_memdump(dumpfile, exploitfile, taint_map, offset = 0, page_size = 4096
             if data == "":
                 break
 
-            if taint_map.has_key(out.tell()):
+            if taint.has_key(data):
                 count = count + 1
                 with open(exploitfile, "rb+") as exploit:
-                    exploit.seek(taint_map[out.tell()])
+                    exploit.seek(taint[data])
                     data = exploit.read(page_size)
                     datalen = len(data)
                     if datalen < page_size:
@@ -54,24 +52,6 @@ def patch_memdump(dumpfile, exploitfile, taint_map, offset = 0, page_size = 4096
         print "Totally patched " + str(count) + " pages"
 
 
-def get_taint_map(taintfile, exploitfile, page_size = 4096):
-    taint_map = {}
-    with open(taintfile, "rb") as taint, open(exploitfile, "rb") as exploit:
-        while True:
-            taint_page = taint.read(page_size)
-            if taint_page == "":
-                break
-
-            res = search_memdump(sys.argv[3], taint_page, 0xc0000000, page_size)
-            if res == []:
-                print "tainted page not found"
-            else:
-                for i in res:
-                    print "find tainted page " + hex(taint.tell() - page_size) + " at " + hex(i)
-                    taint_map[i] = taint.tell() - page_size
-    return taint_map
-
-
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print "Usage: ./patch-memdump.py <taint> <exploit> <memdump>"
@@ -79,8 +59,6 @@ if __name__ == "__main__":
 
     page_size = 4096
     
-    # hashmap(memdump_offset, taint/exploit_offset)
-    taint_map = get_taint_map(sys.argv[1], sys.argv[2], page_size)
-    print taint_map
-    patch_memdump(sys.argv[3], sys.argv[2], taint_map, 0xc0000000, page_size)
+    taint = load_file_to_hashmap(sys.argv[1], page_size)
+    patch_memdump(sys.argv[3], sys.argv[2], taint, 0xc0000000, page_size)
     exit(0)
